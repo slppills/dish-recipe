8ac79fea2b8e9251811e2caa80bc6280d09ac857
slppills <sunchaehoon2@naver.com>
2024-06-28 17:41:23 +0900

    router

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,10 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import Main from "./components/Main/Main";
+import Rice from "./components/Menu/Menu";
+import Dish from "./components/Dish/Dish";
 
 const Router = () => {
 	return (
 		<Routes>
 			<Route path="/" element={<Main />} />
+			<Route path="/:menuType" element={<Rice />} />
+			<Route path="/:menuType/:id" element={<Dish />} />
 		</Routes>
 	);
 };
