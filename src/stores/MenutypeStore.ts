b1bbc2f450b180d116d6da69c5068fdb4ac7da31
slppills <sunchaehoon2@naver.com>
2024-07-01 16:29:30 +0900

    Add menutypeStore

--- src/stores/MenutypeStore.ts
+++ src/stores/MenutypeStore.ts
@@ -1,15 +1,23 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 interface Menutype {
 	menutype: string;
 	setMenutype: (Menutype: string) => void;
 }
 
-const useMenutypeStore = create<Menutype>((set) => ({
-	menutype: "",
-	setMenutype: (newMenutype) => {
-		set(() => ({ menutype: newMenutype }));
-	},
-}));
+const useMenutypeStore = create(
+	persist<Menutype>(
+		(set) => ({
+			menutype: "",
+			setMenutype: (newMenutype) => {
+				set(() => ({ menutype: newMenutype }));
+			},
+		}),
+		{
+			name: "menutype-storage",
+		},
+	),
+);
 
 export default useMenutypeStore;
